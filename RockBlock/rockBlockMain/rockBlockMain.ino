9e28caa59f602a0f704ba309fc203585a0730c26
Colby Cox <coxjc@seas.upenn.edu>
2017-03-23 21:23:09 -0400

    SUBJ: Serial Input MSG: Add serial input and send msg functionality to work w/ it.

--- RockBlock/rockBlockMain/rockBlockMain.ino
+++ RockBlock/rockBlockMain/rockBlockMain.ino
@@ -6,14 +6,15 @@
 #include <SoftwareSerial.h>
 #include "Message/Message.cpp"
 
+
 int rxPin = 10;
 int txPin = 11;
-int sleepPin = 8;
+int sleepPin = 5;
 QueueArray<Message> recMsgs;
 QueueArray<Message> sentMsgs;
 
 SoftwareSerial nss(rxPin, txPin);
-IridiumSBD isbd(nss, sleepPin);
+IridiumSBD isbd(nss);
 uint8_t buffer[200];
 
 void setup()
@@ -26,11 +27,10 @@ void setup()
   //at least 1.
   int signalQuality = -1;
 
-
-
   nss.begin(19200);
 
   isbd.attachConsole(Serial);
+  isbd.attachDiags(Serial);
   isbd.setPowerProfile(0);
   isbd.begin();
 
@@ -63,7 +63,14 @@ void setup()
   //  Serial.println(isbd.getWaitingMessageCount());
 
   //*************************************************//
+
+
+
 }
+
+//char[] hexToChars(uint8_t[] msg) {
+//  for 
+//  } 
 
 //requires a Message object. Reger to the message class
 void sendMessage(Message m)
@@ -119,7 +126,59 @@ void addMessageToSentQueue(Message m)
 
 void loop()
 {
-  
+  bool serialReceived = false;
+  String incomingSerial;
+  int signalQuality = -1;
+  //take a look at the code below for reference of checking signal quality
+  int err = isbd.getSignalQuality(signalQuality);
+  if (err != 0)
+  {
+    Serial.print("SignalQuality failed: error ");
+    Serial.println(err);
+    return;
+  }
+
+
+  if (Serial.available() > 0) {
+                incomingSerial = Serial.readString();
+                Serial.print("I serial received: ");
+                serialReceived = true;
+    } 
+
+
+  // *************************************** //
+  //BELOW IS OLD CODE, USELESS, BUT FEEL FREE TO USE AS REFERNCE
+
+
+  Serial.print("Signal quality is ");
+  Serial.println(signalQuality);
+  if (signalQuality > 0 && serialReceived) {
+
+    char bufferOut[200]; 
+    incomingSerial.toCharArray(bufferOut, 200);
+    uint8_t bufferIn[200];
+    size_t bufferSize = sizeof(bufferIn);
+    err = isbd.sendReceiveSBDText(bufferOut, bufferIn, bufferSize);
+    if (err != 0)
+    {
+      Serial.print("sendReceiveSBDText failed: error ");
+      Serial.println(err);
+      return;
+    }
+
+    Serial.print("Inbound buffer size is ");
+    Serial.println(bufferSize);
+    for (int i = 0; i < bufferSize; ++i)
+    {
+      Serial.write(buffer[i]);
+//      Serial.print("(");
+//      Serial.print(buffer[i], HEX);
+//      Serial.print(") ");
+    }
+    Serial.print("Messages left: ");
+    Serial.println(isbd.getWaitingMessageCount());
+  }
+  serialReceived = false;
 }
 
 
